bf11ae15f7e41dc560a55365ebe19e48457577e6
psh <5FishAlive>
2014-02-20 16:29:38 -0600

    Initial UAT that demonstrates a login

--- src/main/java/com/asynchrony/webdriver/testdata/WordPressLoginPage.java
+++ src/main/java/com/asynchrony/webdriver/testdata/WordPressLoginPage.java
@@ -2,6 +2,7 @@ package com.asynchrony.webdriver.testdata;
 
 public abstract class WordPressLoginPage
 {
-    public static final String idUserNameField = "user_name";
+    public static final String idUserNameField = "user_login";
     public static final String idPasswordField = "user_pass";
+    public static final String idSubmit= "wp-submit";
 }
